bb2048e8174fb929b5c426b4e678de3e136c0101
Rafael.dvieira <rafael.dvieira@outlook.com>
2024-07-05 16:45:18 -0300

    primeiro commit

---
+++ exercicios.js
@@ -0,0 +1,15 @@
+/*for (let contador = 0; contador < 4; contador++) {
+  console.log(contador);
+}*/
+
+/*let contador = 0;
+while (contador <= 5) {
+  console.log(`Número: ${contador}`);
+  contador++;
+}*/
+
+let contador = 0;
+do {
+  console.log("Rafa");
+  contador++;
+} while (contador < 10);
